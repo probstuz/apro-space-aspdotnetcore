8671beb311a4c1aac7c30735ead39ae0da386b0f
probstuz <mail@apro.space>
2021-05-23 10:02:11 +0200

    added project files

---
+++ Controllers/pController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using first.Models;
+
+namespace first.Controllers
+{
+    public class pController : Controller
+    {
+        private readonly ILogger<pController> _logger;
+
+        public pController(ILogger<pController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Coding()
+        {
+            return View();
+        }
+
+        public IActionResult Music()
+        {
+            return View();
+        }
+
+        public IActionResult Addons()
+        {
+            return View();
+        }
+
+        public IActionResult Unity()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
